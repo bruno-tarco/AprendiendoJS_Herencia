15686a14092381c2373b60bd0f4e0d38cd613539
Bruno Tarco <bruno_mauricio123@hotmail.com>
2023-02-09 17:01:23 -0500

    actualizacion de polimorfismo

--- SistemaAutenticacion.js
+++ SistemaAutenticacion.js
@@ -3,7 +3,8 @@ export class SistemaAutenticacion{
    
     static login(usuario,clave){
 
-        return usuario.clave == clave;
+        return usuario.clave == clave; // comprueba la autenticación de cualquiér método que tenga el atributo clave
+        
        
     }
 }
