7f23dd134f6df61cb7e4bc519073168fcfd64aa3
Bruno Tarco <bruno_mauricio123@hotmail.com>
2023-02-09 17:24:16 -0500

    interfaces

--- SistemaAutenticacion.js
+++ SistemaAutenticacion.js
@@ -2,9 +2,7 @@ export class SistemaAutenticacion{
    
    
     static login(usuario,clave){
-
-        return usuario.clave == clave; // comprueba la autenticación de cualquiér método que tenga el atributo clave
-        
+        return usuario.autenticable(clave);        
        
     }
 }
--- index.js
+++ index.js
@@ -5,10 +5,8 @@ import { Empleado } from './Empleados/Empleado.js';
 import { Gerente } from './Empleados/Gerente.js';
 import { SistemaAutenticacion } from './SistemaAutenticacion.js';
 
-const cliente = new Cliente('Leonardo','13804050','123224');
-cliente.asignarClave('01111');
-console.log(SistemaAutenticacion.login(cliente, '1111'));
-const cliente2 = new Cliente('María','16979808','8989');
+
+//const cliente2 = new Cliente('María','16979808','8989');
 
 const empleado = new Empleado('Juan Perez','1234343',10000);
 empleado.asignarClave('12345');
@@ -17,12 +15,15 @@ console.log(SistemaAutenticacion.login(empleado,'12345'));
 
 const gerente = new Gerente('Pedro Rivas','232344',12000);
 gerente.asignarClave('655');
-console.log(SistemaAutenticacion.login(gerente,'6556'));
+console.log(SistemaAutenticacion.login(gerente,'655'));
 
 
-const director = new Director('Elene Moreno','232323',15000);
+//const director = new Director('Elene Moreno','232323',15000);
 
 
+const cliente = new Cliente('Leonardo','13804050','123224');
+cliente.asignarClave('01111');
+console.log(SistemaAutenticacion.login(cliente, '1111'));
 
 
 
